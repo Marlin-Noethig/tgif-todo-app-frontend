b17eeb0ddededc3443ecea1ee127e2788c1c9f48
Marlin Noethig <marlin3003@gmx.de>
2022-04-27 14:46:24 +0200

    finished details-form

--- frontend/src/components/EditDetails.tsx
+++ frontend/src/components/EditDetails.tsx
@@ -8,24 +8,23 @@ type EditDetailsProps = {
 }
 
 
-
-
-export default function EditDetails({todo, toggleCreate}:EditDetailsProps){
+export default function EditDetails({todo, toggleCreate}: EditDetailsProps) {
     const [description, setDescription] = useState(todo.description)
     const [status, setStatus] = useState(todo.status)
 
 
-    return(
+    return (
         <div>
             <form>
                 <label>Edit description:
                     <input type={"text"}
                            value={description}
-                            onChange={e => setDescription(e.target.value)}/>
+                           onChange={e => setDescription(e.target.value)}/>
                 </label>
                 <label>Edit status:
                     <select value={status} onChange={e => setStatus(e.target.value)}>
-                        {StatusConfig.statuses.map(status => <option value={status}>{StatusConfig.renderStatus(status)}</option>)}
+                        {StatusConfig.statuses.map(status => <option
+                            value={status}>{StatusConfig.renderStatus(status)}</option>)}
                     </select>
                 </label>
                 <input type={"submit"} onClick={toggleCreate} value={"save"}/>
